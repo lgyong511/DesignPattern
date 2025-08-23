778b37faac7c4bce6898f21b45233f2dc68ce8f7
lgyong <lgyong511@163.com>
2025-08-23 15:02:36 +0800

    原型模式

--- CreationalPatterns/6.Prototype/Prototype.go
+++ CreationalPatterns/6.Prototype/Prototype.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // 原型模式
 
 /* 它通过复制现有对象来创建新对象，而不是通过新建类的方式。原型模式的核心思想是利用对象的克隆能力，避免重复初始化，特别适用于创建成本较高的对象。
@@ -14,3 +16,50 @@ package main
 
 这需要根据需求选择适当的拷贝方式
 */
+
+// 原型接口
+// 定义一个克隆方法，用于创建新的对象
+type Prototyper interface {
+	Clone() Prototyper
+}
+
+type Prototype struct {
+	Name string
+	Age  int
+}
+
+// 实现克隆方法
+func (p *Prototype) Clone() Prototyper {
+	return &Prototype{
+		Name: p.Name,
+		Age:  p.Age,
+	}
+}
+
+// 展示
+func (p *Prototype) Show() {
+	fmt.Printf("Name: %s, Age: %d\n", p.Name, p.Age)
+}
+
+func main() {
+	// 创建一个原型对象
+	prototype := &Prototype{
+		Name: "张三",
+		Age:  18,
+	}
+
+	// 克隆原型对象
+	clone := prototype.Clone().(*Prototype)
+
+	// 展示克隆对象
+	clone.Show()
+
+	// 修改克隆对象的属性
+	clone.Name = "李四"
+	clone.Age = 20
+
+	// 展示修改后的克隆对象
+	clone.Show()
+	// 展示原型对象
+	prototype.Show()
+}
